173344389c003d6d89f630e5fc834f738d410eee
Mark Oborne <markoborne1@gmail.com>
2022-04-02 19:04:03 +0100

    chore: prepares repo for start of project

--- test/catch_main.cpp
+++ test/catch_main.cpp
@@ -1,3 +1,8 @@
 #define CATCH_CONFIG_MAIN// This tells the catch header to generate a main
 
 #include <catch2/catch.hpp>
+
+TEST_CASE("Example test case")
+{
+    SECTION("Ensure 0 is not equal to 1") { REQUIRE(0 != 1); }
+}
